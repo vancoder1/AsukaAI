1151dd39b8646a259ffa6f74b8b5595d958b6921
vancoder1 <ivanzaporozhets25@gmail.com>
2023-09-06 23:27:50 -0600

    Change interface and add some interactive choice for future

---
+++ aidata.py
@@ -0,0 +1,46 @@
+import os
+import json
+from gpt4all import GPT4All
+
+class AI_Data:
+    name : str
+    age : int
+    type : str
+    system_template : str
+    temperature : float
+    max_tokens : int
+    top_p : float
+    repeat_penalty : float
+
+#init local variables for AI_Data class from config.json file
+def init_variables(modelNum):
+    try:
+        with open(os.path.dirname(__file__) + "\config.json", "r") as json_file:
+            data = json.load(json_file)
+    except:
+        print("Unable to open JSON file. File was not found or can be corrupted.")
+        exit()
+    
+    # 0 - EvilChan(Usually rude and toxic), 1 - Hinata(Your true waifu, speaks like you are a couple),
+    # 2 - LalaChan(Will speak with you like you are friends)
+    modelNumber = modelNum
+
+    aidata = AI_Data()
+    aidata.name = data["AI_data"][modelNumber]["model_name"]
+    aidata.age = data["AI_data"][modelNumber]["model_age"]
+    aidata.type = data["AI_data"][modelNumber]["model_type"]
+    aidata.system_template = data["AI_data"][modelNumber]["system_template"]
+    aidata.temperature = data["AI_data"][modelNumber]["temperature"]
+    aidata.max_tokens = data["AI_data"][modelNumber]["max_tokens"]
+    aidata.top_p = data["AI_data"][modelNumber]["top_p"]
+    aidata.repeat_penalty = data["AI_data"][modelNumber]["repeat_penalty"]
+    return aidata
+class WaifuAI:
+    data : AI_Data
+    model : GPT4All
+    model_choice : int
+    def __init__(self, model_ch):
+        self.model_choice = model_ch
+        self.data = init_variables(self.model_choice)
+        self.model = GPT4All(os.path.dirname(__file__) + 
+                    "\models\llama-2-7b-chat.ggmlv3.q4_0.bin", allow_download=False)
--- main.py
+++ main.py
@@ -1,77 +1,49 @@
 import os
 import json
-import time
-from colorama import init, Fore, Back, Style
-from gpt4all import GPT4All
-import speech_recognition
-
-class AI_Data:
-    name : str
-    age : int
-    type : str
-    system_template : str
-    temperature : float
-    max_tokens : int
-    top_p : float
-    repeat_penalty : float
-
-#init local variables for AI_Data class from config.json file
-def init_variables():
-    try:
-        with open(os.path.dirname(__file__) + "\config.json", "r") as json_file:
-            data = json.load(json_file)
-    except:
-        print("Unable to open JSON file. File was not found or can be corrupted.")
-        exit()
-    
-    # 0 - EvilChan, 1 - Hinata
-    modelNumber = 1
-    aidata = AI_Data()
-    aidata.name = data["AI_data"][modelNumber]["model_name"]
-    aidata.age = data["AI_data"][modelNumber]["model_age"]
-    aidata.type = data["AI_data"][modelNumber]["model_type"]
-    aidata.system_template = data["AI_data"][modelNumber]["system_template"]
-    aidata.temperature = data["AI_data"][modelNumber]["temperature"]
-    aidata.max_tokens = data["AI_data"][modelNumber]["max_tokens"]
-    aidata.top_p = data["AI_data"][modelNumber]["top_p"]
-    aidata.repeat_penalty = data["AI_data"][modelNumber]["repeat_penalty"]
-    return aidata
-
+from colorama import init, Fore, Style
+import whisper
+import aidata
+       
 def speechToText():
-    recognizer = speech_recognition.Recognizer()
-    with speech_recognition.Microphone() as mic:
-        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-        audio = recognizer.listen(mic)
-
-        text = recognizer.recognize_google(audio)
-        text = text.lower()
-
-        return text
-
-class WaifuAI:
-    data : AI_Data
-    model : GPT4All
-    def __init__(self):
-        self.data = init_variables()
-        self.model = GPT4All(os.path.dirname(__file__) + 
-                    "\models\llama-2-7b-chat.ggmlv3.q4_0.bin")
-
+    pass
 
 def main():
     # TOKEN = os.getenv("DISCORD_TOKEN")
     # GUILD = os.getenv("DISCORD_GUILD")
     init(autoreset=True)
 
-    AwwWaifuAI = WaifuAI()
+    # Model and conversation type choosing
+    print(Fore.MAGENTA + Style.BRIGHT + "Choose your model:")
+    print(Fore.MAGENTA + "1. EvilChan - usually rude and toxic")
+    print(Fore.MAGENTA + "2. HinataChan - your true waifu, thinks that you are a couple")
+    print(Fore.MAGENTA + "3. LalaChan - will speak with you like you are friends")
+    modelChoice = int(input(Fore.MAGENTA + "Type a number of your choice: "))
+    print()
+    if (modelChoice < 1 or modelChoice > 3):
+        modelChoice = 3
+    modelChoice -= 1
 
-    print(Fore.MAGENTA + Style.BRIGHT + "You will speak to " + AwwWaifuAI.data.name)
-    print(Fore.MAGENTA + Style.BRIGHT + "Enter \"exit\" to stop the conversation")
+    print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "Now choose the type of comunicatiton:")
+    print(Fore.LIGHTGREEN_EX + "1. Text conversation")
+    print(Fore.LIGHTGREEN_EX + "2. Voice conversation")
+    convTypeChoice = int(input(Fore.LIGHTGREEN_EX + "Type a number of your choice: "))
+    if (convTypeChoice < 1 or convTypeChoice > 2):
+        convTypeChoice = 1
+
+    # AI code
+    AwwWaifuAI = aidata.WaifuAI(modelChoice)
+    print(Fore.MAGENTA + Style.BRIGHT + "You will speak with " + AwwWaifuAI.data.name)
+    print(Fore.MAGENTA + Style.BRIGHT + "Enter \"exit\" to stop the conversation\n")
     conversation_flag = True
 
     with AwwWaifuAI.model.chat_session(system_prompt=AwwWaifuAI.data.system_template):
         while conversation_flag:
             tokens = []
-            your_prompt = str(input(Fore.CYAN + "YOU: "))
+            if (convTypeChoice == 1):
+                your_prompt = str(input(Fore.CYAN + "YOU: "))
+            elif(convTypeChoice == 2):
+                # your_prompt = speechToText()
+                print(Fore.CYAN + "YOU: " + your_prompt)
             
             if (str.lower(your_prompt) == "exit"):
                 conversation_flag = False
