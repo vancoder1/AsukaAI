eca82e3663a6431f4977a0ee499dd20a48322f70
vancoder1 <ivanzaporozhets25@gmail.com>
2023-09-06 21:15:13 -0600

    Just a usual commit to merge branches

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from colorama import init, Fore, Back, Style
 from gpt4all import GPT4All
 import speech_recognition
+
 class AI_Data:
     name : str
     age : int
@@ -14,6 +15,7 @@ class AI_Data:
     top_p : float
     repeat_penalty : float
 
+#init local variables for AI_Data class from config.json file
 def init_variables():
     try:
         with open(os.path.dirname(__file__) + "\config.json", "r") as json_file:
@@ -23,7 +25,7 @@ def init_variables():
         exit()
     
     # 0 - EvilChan, 1 - Hinata
-    modelNumber = 1
+    modelNumber = 0
     aidata = AI_Data()
     aidata.name = data["AI_data"][modelNumber]["model_name"]
     aidata.age = data["AI_data"][modelNumber]["model_age"]
@@ -36,7 +38,15 @@ def init_variables():
     return aidata
 
 def speechToText():
-    pass
+    recognizer = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as mic:
+        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+        audio = recognizer.listen(mic)
+
+        text = recognizer.recognize_google(audio)
+        text = text.lower()
+
+        return text
 
 class WaifuAI:
     data : AI_Data
@@ -55,13 +65,16 @@ def main():
     AwwWaifuAI = WaifuAI()
     print("Enter \"exit\" to stop the conversation")
     conversation_flag = True
+
     with AwwWaifuAI.model.chat_session(system_prompt=AwwWaifuAI.data.system_template):
         while conversation_flag:
             tokens = []
             your_prompt = str(input(Fore.CYAN + "YOU: "))
+            
             if (str.lower(your_prompt) == "exit"):
                 conversation_flag = False
                 break
+
             for token in AwwWaifuAI.model.generate(prompt=your_prompt, max_tokens=AwwWaifuAI.data.max_tokens, streaming=True):
                 tokens.append(token)
                 print(Fore.LIGHTRED_EX + str(token), end='')
